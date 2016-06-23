24ec531f1f8fdd6efb5c02f7c176b64405273bc1
swingcat <chenlinxi1991@gmail.com>
2016-06-23 16:13:21 +0800

    layout initial commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -114,26 +114,7 @@ gulp.task('styles', () => {
 // `.babelrc` file.
 gulp.task('scripts', () =>
     gulp.src([
-      // Component handler
-      './app/styles/src/mdlComponentHandler.js',
-      // Base components
-      './app/styles/src/button/button.js',
-      './app/styles/src/checkbox/checkbox.js',
-      './app/styles/src/icon-toggle/icon-toggle.js',
-      './app/styles/src/menu/menu.js',
-      './app/styles/src/progress/progress.js',
-      './app/styles/src/radio/radio.js',
-      './app/styles/src/slider/slider.js',
-      './app/styles/src/spinner/spinner.js',
-      './app/styles/src/switch/switch.js',
-      './app/styles/src/tabs/tabs.js',
-      './app/styles/src/textfield/textfield.js',
-      './app/styles/src/tooltip/tooltip.js',
-      // Complex components (which reuse base components)
-      './app/styles/src/layout/layout.js',
-      './app/styles/src/data-table/data-table.js',
-      // And finally, the ripples
-      './app/styles/src/ripple/ripple.js',
+      
       // Other scripts,
       './app/scripts/main.js'
     ])
